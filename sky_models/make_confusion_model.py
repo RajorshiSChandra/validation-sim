b9893cad7f74584b5599687589c352b134df677d
Piyanat (Boom) Kittiwisit <piyanat.kittiwisit@gmail.com>
2023-04-26 09:37:02 +0200

    feat: update to match feat of other sky model code

--- sky_models/make_confusion_model.py
+++ sky_models/make_confusion_model.py
@@ -1,80 +1,102 @@
-"""Make source confusion SkyModel using healpy gridding."""
-from multiprocessing import Pool, Array
-import numpy as np
+from multiprocessing import Pool
+import argparse
+import os
+
 import healpy as hp
+import numpy as np
 from pyradiosky import SkyModel
 from astropy import units as u
 
+import utils
 
-# Load data
-with np.load('./gleam_like/gleam_like_fainter.npz') as data:
-    flux_ref = data['flux_density']
-    colat = data['theta']
-    lon = data['phi']
-    spectral_index = data['spec_index']
+# H4C frequencies
+FREQ_ARRAY = utils.freqs
 
 
-# Some parameters
-ref_freq = 154e6
-freq_array = np.arange(1e8, 2e8, 97656.25)
-nfreqs = freq_array.size
-nside = 128
-npix = hp.nside2npix(nside)
+def make_confusion_map(f, nside=128):
+    """Make a confusion map at a given frequency. Output NSIDE=128."""
+    # Load source confusion data
+    ref_freq = 154e6
+    data_file = f"{utils.SKYDIR}/gleam_like/gleam_like_fainter.npz"
 
-# Pixel indices of sources
-pixel_inds = hp.ang2pix(nside, colat, lon, nest=False)
+    with np.load(data_file) as data:
+        flux_ref = data["flux_density"]
+        colat = data["theta"]
+        lon = data["phi"]
+        spectral_index = data["spec_index"]
 
-# Shared memory array for healpix fluxes
-mp_arr = Array('d', nfreqs * npix)
-# sum_flux = np.zeros((freq_array.size, npix)
+    # Pixel indices of confusion sources
+    pixel_inds = hp.ang2pix(nside, colat, lon, nest=False)
 
-
-# Multiprocessing over frequency, loop over pixel indices, 
-# add source fluxes to healpix pixels
-def assign_to_healpix(i):
-    f = freq_array[i]
+    # Power law interpolation to get source fluxes at the given frequency.
     flux_f = flux_ref * (f / ref_freq) ** spectral_index
-    arr = np.frombuffer(mp_arr.get_obj())
-    arr.shape = (nfreqs, npix)
-    for j, p in enumerate(pixel_inds):
-        arr[i, p] += flux_f[j]
 
-        
-with Pool() as p:
-    p.map(assign_to_healpix, range(nfreqs))
+    # Initialize confusion map
+    confusion_map = np.zeros(hp.nside2npix(nside))
 
-    
+    # Loop over pixel indices of the sources and add fluxes to the right pixels.
+    for i, p in enumerate(pixel_inds):
+        confusion_map[p] += flux_f[i]
 
-# Load the sum_flux from shared memory
-sum_flux = np.frombuffer(mp_arr.get_obj())
-sum_flux.shape = (nfreqs, npix)
+    # Divde by pixel area to conver to Jy/sr
+    confusion_map = confusion_map / hp.nside2pixarea(nside)
 
-# Divde by pixel area to conver to Jy/sr
-sum_flux = sum_flux / hp.nside2pixarea(nside)
+    # Now convert from Jy/sr to kelvin
+    confusion_map = (
+        (confusion_map * u.Jy / u.sr)
+        .to(u.K, equivalencies=u.brightness_temperature(f * u.Hz))
+        .value
+    )
 
-# Save the array 
-np.save('./gleam_like/gleam_like_fainter_healpix_sum_nside128.npy', sum_flux)
+    return confusion_map
 
-# This part kill the job maybe OOM
-# # Build a SkyModel
-# # Stokes must be in Jy/sr or K, so dividing by pixel area in sr
-# stokes = np.zeros((4, nfreqs, npix)) * u.Jy / u.sr
-# stokes[0, :, :] = sum_flux * u.Jy / u.sr
-# params = {
-#     'component_type': 'healpix',
-#     'nside': nside,
-#     'hpx_inds': np.arange(npix),
-#     'hpx_order': 'ring',
-#     'spectral_type': 'full',
-#     'freq_array': freq_array * u.Hz,
-#     'stokes': stokes,
-#     'history': ' Generate a gleam-like confusion map.\n'
-# }
-# confusion_model = SkyModel(**params)
 
-# # Convert to Kelvin
-# confusion_model.jansky_to_kelvin()
+def make_sky_model(fch):
+    print(f"{os.getpid()}: computing channel ", fch)
 
-# # Save
-# confusion_model.write_skyh5('gleam_like_fainter_healpix_sum_nside128.sky5h',
-#                             clobber=True)
+    f = FREQ_ARRAY[fch]
+
+    print(f"{os.getpid()}: getting confusion map ")
+    confusion_map = make_confusion_map(f, nside=out_nside)
+
+    # Build a SkyModel object
+    # Output is per frequency, so the second dimension is 1.
+    stokes = np.zeros((4, 1, out_npix)) * u.K
+    stokes[0, :, :] = confusion_map * u.K
+    # SkyModel parameters.
+    # The frame parameter defines the coordinate frame of the map.
+    params = {
+        "component_type": "healpix",
+        "nside": out_nside,
+        "hpx_inds": np.arange(out_npix),
+        "hpx_order": "ring",
+        "spectral_type": "full",
+        "freq_array": f * u.Hz,
+        "stokes": stokes,
+        "history": "Generate a confusion model with gleam-like sources.\n",
+        "frame": "galactic",
+    }
+    confusion_model = SkyModel(**params)
+
+    # Convert healpix to point
+    confusion_model.healpix_to_point()
+
+    # Transform coordinates of the GSM pixels from Galactic to ICRS.
+    confusion_model.transform_to("icrs")
+
+    utils.write(confusion_model, f"confusion_nside{out_nside}", fch)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create Confusion SkyModel.")
+    parser.add_argument(
+        "--nside", type=int, default=128, help="output NSIDE of the healpix maps."
+    )
+    parser.add_argument("--ncores", type=int, default=None, help="number of processes.")
+    args = parser.parse_args()
+
+    out_nside = args.nside
+    out_npix = hp.nside2npix(out_nside)
+
+    with Pool(args.ncores) as p:
+        p.map(make_sky_model, range(FREQ_ARRAY.size))
