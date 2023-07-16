3b3abf71731ea23ffb18d6ee13b5fb2384f12c98
Piyanat Kittiwisit <piyanat.kittiwisit@gmail.com>
2023-07-16 02:20:21 +0200

    feat: add several options and change handling of hpc config

--- run_sim.py
+++ run_sim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+import subprocess
 from pathlib import Path
 import logging
 import numpy as np
@@ -7,45 +7,38 @@ import yaml
 import click
 import utils
 
-H4C_FREQS = utils.h4c_freqs
+H4C_FREQS = utils.H4C_FREQS
 REPODIR = utils.REPODIR
 
-CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"], max_content_width=100)
+
+logger = logging.getLogger(__name__)
 
 
-def _get_sbatch(hpc_config: str | Path):
+def _get_sbatch_program(hpc_config: str | Path, slurm_override=None):
     with open(hpc_config, "r") as fl:
         hpc_params = yaml.safe_load(fl)
     conda_params = hpc_params["conda"]
+    module_params = hpc_params["module"]
     slurm_params = hpc_params["slurm"]
-    # If H4c
-    slurm_params["log_dir"] = "logs"
-    slurm_params["log_name"] = "job_name"
-    module = hpc_params["module"]
+    if slurm_override is not None:  # Modify slurm options
+        for k, v in slurm_override:
+            slurm_params[k] = v
 
-    sbatch_header = """#!/bin/bash
-#SBATCH -o {log-dir}/{log-name}-%j.out
-#SBATCH --job-name={job-name}
-#SBATCH --partition={partition}
-#SBATCH --mem={mem}GB
-#SBATCH --nodes={nodes}
-#SBATCH --ntasks={ntasks}
-#SBATCH --cpus-per-task={cpus-per-task}
-#SBATCH --time={time}
-""".format_map(
-        slurm_params
-    )
+    shebang = "#!/bin/bash"
+    sbatch = "\n".join([f"#SBATCH --{k}={v}" for k, v in slurm_params.items()])
 
-    conda_setup = """source {conda_path}/bin/activate
+    conda = """
+source {conda_path}/bin/activate
 conda activate {environment_name}
 """.format_map(
         conda_params
     )
 
-    module_setup = "".join([f"module load {md}\n" for md in module])
+    module = "\n".join([f"module load {md}" for md in module_params])
 
-    sbatch = "\n".join([sbatch_header, conda_setup, module_setup])
-    return sbatch
+    program = "\n".join([shebang, sbatch, conda, module])
+    return program
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -61,8 +54,9 @@ def run_sim():
     nargs=2,
     default=(0, len(H4C_FREQS)),
     show_default=True,
+    metavar="START STOP",
     type=click.IntRange(0, len(H4C_FREQS)),
-    help="Frequency channel range to simulate",
+    help="Frequency channel range (Pythonic)",
 )
 @click.option(
     "-fch",
@@ -70,6 +64,7 @@ def run_sim():
     multiple=True,
     default=None,
     type=click.IntRange(0, len(H4C_FREQS)),
+    metavar="FREQ_CHAN",
     help="Frequency channel, allow multiple, add to --freq-range",
 )
 @click.option(
@@ -77,42 +72,84 @@ def run_sim():
     "--sky_model",
     default="ptsrc",
     show_default=True,
-    type=click.Choice(["ptsrc", "diffuse_nside128", "eor"], case_sensitive=True),
+    type=click.Choice(["ptsrc", "diffuse_nside256", "eor"], case_sensitive=True),
     help="Sky model to simulate",
 )
 @click.option(
     "--chunks",
     default=3,
     show_default=True,
-    help="Split simulation into a number of time chunks",
+    help="Split the simulation into a number of time chunks",
 )
 @click.option("--gpu/--cpu", default=False, show_default=True, help="Use gpu or cpu")
 @click.option(
-    "--clobber", is_flag=False, help="Do not check if outputs exist and overwrite"
-)
-# @click.option("--runtime", default=None, help="time string for slurm sbatch")
-# @click.option("--mem", default=None, help="mem in GB for slurm sbatch")
-# @click.option("--ntasks", default=None, help="ntasks for slurm sbatch")
-# @click.option("--cpus-per-task", default=None, help="cpus per task for slurm sbatch")
-@click.option(
     "--hpc-config",
     type=click.Path(exists=True, resolve_path=True),
-    help="HPC config yaml file, run hera-sim-vis.py locally if not provided",
+    help="HPC config YAML file for creating an SBATCH script "
+    "to run on a cluster; run locally if not provided",
 )
-def h4c(freq_range, freqs, sky_model, chunks, gpu, clobber, hpc_config):
-    """Run H4C validation simulations.
+@click.option(
+    "-so",
+    "--slurm-override",
+    nargs=2,
+    multiple=True,
+    metavar="FLAG VALUE",
+    help="Override slurm options in the hpc config (excluding job-name and output), "
+    "allow multiple",
+)
+@click.option(
+    "--log-level",
+    default="WARNING",
+    type=click.Choice(["INFO", "WARNING", "ERROR", "DEBUG"], case_sensitive=True),
+    show_default=True,
+    help="Verbosity level, also pass the flag to hera-sim-vis.py",
+)
+@click.option(
+    "--clobber/--skip-existing-output",
+    default=False,
+    show_default=True,
+    help="Run the simulation and overwrite regardless of wheather the output already "
+    "exists, or skip it",
+)
+@click.option("-d", "--dry-run", is_flag=True, help="Pass the flag to hera-sim-vis.py")
+def h4c(
+    freq_range,
+    freqs,
+    sky_model,
+    chunks,
+    gpu,
+    hpc_config,
+    slurm_override,
+    clobber,
+    log_level,
+    dry_run,
+):
+    """Make a job script to run H4C validation sims via hera-sim-vis.py.
 
-    Simulation parameters are fixed to
-    * Vivaldi beam, extrapolated to cover 45-50 MHz
-    * Beam interpolation: cubic in frequency, linear in spatail
-    * Frequencies. See freqs.yaml
+    Use the default parameters, configuration files, and directories for H4C sims
+    (see make_obsparams.py).
     """
-    out_dir = REPODIR / f"outputs/{sky_model}/"
-    config_dir = REPODIR / f"config_files/obsparams/{sky_model}/nt17280_spl{chunks}"
-    simulator_config = REPODIR / "viscpu.yaml" if gpu else REPODIR / "viscpu.yaml"
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s", level=getattr(logging, log_level)
+    )
 
-    sbatch = _get_sbatch(hpc_config) if hpc_config else ""
-    hera_sim_options = "--normalize_beams --fix_autos --log-level INFO"
+    out_dir = REPODIR / f"outputs/{sky_model}/nt17280/"
+    config_dir = REPODIR / f"config_files/obsparams/{sky_model}/nt17280_spl{chunks}"
+    simulator_config = REPODIR / "visgpu.yaml" if gpu else REPODIR / "viscpu.yaml"
+
+    # We want to override the job-name to be <sky_model>-<fch>-<ch>, but the last two
+    # varaibles have to be accessed in the loop, so we will be instead override it to
+    # a Python string formatting pattern and format it in the loop.
+    # Note that click default `slurm_overrride` to (), and we want it to be "2D" tuple
+    slurm_override = slurm_override + (
+        ("job-name", "{sky_model}-fch{fch:04d}-chunk{ch}"),
+    )
+    # Make the SBATCH script minus hera-sim-vis.py command
+    program = _get_sbatch_program(hpc_config, slurm_override) if hpc_config else ""
+
+    # Option for hera-sim-vis.py. Let's just keep this fixed.
+    sim_options = f"--normalize_beams --fix_autos --log-level {log_level}"
+    sim_options += " --dry-run" if dry_run else ""
 
     freq_chans = np.arange(*freq_range)
     if freqs:
@@ -124,35 +161,37 @@ def h4c(freq_range, freqs, sky_model, chunks, gpu, clobber, hpc_config):
             outfile = out_dir / f"{sky_model}_fch{fch:04d}_nt17280_chunk{ch}.uvh5"
             # Check if output file already existed, if clobber is False
             if not clobber and outfile.exists():
-                logging.warn(f"File {outfile} exists, skipping")
+                logger.warning(f"File {outfile} exists, skipping")
             else:
-                # Check if obsparam exist
+                # TODO: Add an option to skip or remake obsparam
                 # TODO: later remove nt1780
                 obsparam = config_dir / f"fch{fch:04d}_chunk{ch}.yaml"
                 if not obsparam.exists():
-                    logging.warn(f"{obsparam} does not exist, skipping")
+                    logger.warning(f"{obsparam} does not exist, skipping")
                     # TODO: later add option to automatically make obsparam
                 else:
-                    # Make sbatch script
-                    cmd = f"hera-sim-vis.py {hera_sim_options} {obsparam} {simulator_config}"
+                    cmd = f"hera-sim-vis.py {sim_options} {obsparam} {simulator_config}"
                     if hpc_config:
-                        job_script = "\n".join([sbatch, cmd])
-                        print(job_script)
-                        # Submit job
-                        # with open("_sim.sbatch", "w") as fl:
-                        # fl.write(job_script)
+                        # Now, join the job script with the hera-sim-vis.py command
+                        # and format the job-name
+                        job_script = "\n".join([program, "", cmd, ""]).format(
+                            sky_model=sky_model, fch=fch, ch=ch
+                        )
+                        # Write job script and submit
+                        # Maybe we should save each job script individually
+                        with open("_sim.sbatch", "w") as fl:
+                            fl.write(job_script)
 
-                        # os.system("sbatch _sim.sbatch")
+                        subprocess.call(
+                            "sbatch _sim.sbatch".split()
+                        ) if not dry_run else None
+                        logger.info(f"\n===Job Script===\n{job_script}\n===END===\n")
                     else:
-                        os.system(f"{cmd}")
+                        logger.info("Running the simulation locally\nCommand: {cmd}")
+                        subprocess.call(cmd.split()) if not dry_run else None
 
 
 @run_sim.command("custom")
-@click.option(
-    "--log-level",
-    default="INFO",
-    type=click.Choice(["INFO", "WARNING", "ERROR", "DEBUG"], case_sensitive=True),
-)
 def custom(log_level):
     """Run custom simulations (not yet implemented).
 
