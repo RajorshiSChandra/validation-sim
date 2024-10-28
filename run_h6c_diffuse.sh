bdc848fdc66952dcdfff4c99083fe145ba2ab7bf
Steven Murray <steven.g.murray@asu.edu>
2024-10-28 11:43:11 -0400

    updated run_h6c scripts

--- run_h6c_diffuse.sh
+++ run_h6c_diffuse.sh
@@ -4,12 +4,12 @@
 #  --slurm-override time '02:30:00' --slurm-override partition RM  \
 #  --sky-model gsm_nside1024 --spline-interp-order 1 \
 #  --prefix final \
-#  --n-time-chunks 1 --do-time-chunks 0 --channels 0~1300
+#  --n-time-chunks 1 --do-time-chunks 0 --channels 0~1200
 
 ./vsim.py runsim \
   --log-level INFO --skip-existing --simulator fftvis32 --force-remake-obsparams \
   --redundant --layout FULL \
-  --slurm-override time '05:00:00' --slurm-override partition RM  \
+  --slurm-override time '06:00:00' --slurm-override partition RM  \
   --sky-model gsm_nside1024 --spline-interp-order 1 \
   --prefix final \
-  --n-time-chunks 1 --do-time-chunks 0 --channels 1300~1536
+  --n-time-chunks 1 --do-time-chunks 0 --channels 1200~1536
--- run_h6c_ptsrc.sh
+++ run_h6c_ptsrc.sh
@@ -1,15 +1,15 @@
-./vsim.py runsim \
-  --log-level INFO --skip-existing --simulator fftvis64 \
-  --redundant --layout FULL \
-  --slurm-override time '03:00:00' --slurm-override partition RM  \
-  --sky-model ptsrc1024 --spline-interp-order 1 \
-  --prefix final \
-  --n-time-chunks 1 --do-time-chunks 0 --channels 100~1200
+#./vsim.py runsim \
+#  --log-level INFO --skip-existing --simulator fftvis64 \
+#  --redundant --layout FULL \
+#  --slurm-override time '03:00:00' --slurm-override partition RM  \
+#  --sky-model ptsrc1024 --spline-interp-order 1 \
+#  --prefix final \
+#  --n-time-chunks 1 --do-time-chunks 0 --channels 100~1200
 
 ./vsim.py runsim \
   --log-level INFO --skip-existing --simulator fftvis32 \
   --redundant --layout FULL \
-  --slurm-override time '05:00:00' --slurm-override partition RM  \
+  --slurm-override time '06:00:00' --slurm-override partition RM  \
   --sky-model ptsrc1024 --spline-interp-order 1 \
   --prefix final \
   --n-time-chunks 1 --do-time-chunks 0 --channels 1200~1536
